18188e139c22920ed4b1b822ab981abf43dc2cfe
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-02-06 19:15:47 +0800

    Revert "fix: Report visibility"
    This reverts commit 0e685e421f5057df685aa917af5f67a4e392ad51.

--- src/components/view/InfoBox.vue
+++ src/components/view/InfoBox.vue
@@ -34,7 +34,7 @@ onMounted(() => {});
 </script>
 
 <template lang="pug">
-.info-box-wrapper(:class="{hidden: currentView == 'report' || currentView == 'report-list'}")
+.info-box-wrapper
   .info-box(:class="{[currentEewIndex ? InfoBoxStatusClass[eew[currentEewIndex].status] : '']: true, show: currentView == 'home'}")
     .header
       .header-title(v-if="currentEewIndex") 地震速報 ｜ {{ eew[currentEewIndex].source.toUpperCase() }}{{ InfoBoxStatusText[eew[currentEewIndex].status] }}
@@ -70,10 +70,6 @@ onMounted(() => {});
   min-width: 300px;
   width: 20vw;
   z-index: 5000;
-
-  &.hidden {
-    visibility: hidden !important;
-  }
 
   > .info-box {
     top: 0;
