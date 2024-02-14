c52f5d7385fcaf80d7021300a29d2ad02527ac6f
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-02-15 04:49:22 +0800

    feat: make zooming back

--- src/components/component/MapEew.vue
+++ src/components/component/MapEew.vue
@@ -5,6 +5,7 @@ import { onMounted, onUnmounted, ref } from "vue";
 import maplibregl from "maplibre-gl";
 
 import type { EewEvent } from "../../types";
+import { kmToPixels } from "../../scripts/helper/utils";
 
 const props = defineProps<{
   map: maplibregl.Map;
@@ -26,11 +27,44 @@ onMounted(() => {
 
   props.map.setLayoutProperty("town", "visibility", "visible");
   props.map.setLayoutProperty("county", "visibility", "none");
+
+  // FIXME: new algorithm for calculating zoom scale
+  const focusEew = () => {
+    const bounds = new maplibregl.LngLatBounds();
+    const zoom = props.map.getZoom();
+
+    for (const id in props.eew) {
+      if (!props.eew[id].detail) {
+        bounds.extend([props.eew[id].lng, props.eew[id].lat]);
+        continue;
+      }
+
+      const center = props.map.project([props.eew[id].lng, props.eew[id].lat]);
+      const radius = kmToPixels(props.eew[id].r.p, props.eew[id].lat, zoom);
+
+      bounds.extend(
+        props.map.unproject([center.x - radius, center.y - radius])
+      );
+      bounds.extend(
+        props.map.unproject([center.x + radius, center.y + radius])
+      );
+    }
+
+    props.map.fitBounds(bounds, {
+      padding: { top: 0, right: 320, bottom: 0, left: 0 },
+      maxZoom: 8,
+    });
+  };
+
+  focusEew();
+
+  intervals.focus = window.setInterval(focusEew, 5_000);
 });
 
 onUnmounted(() => {
   window.clearInterval(intervals.wave);
   window.clearInterval(intervals.cross);
+  window.clearInterval(intervals.focus);
 
   props.map.setLayoutProperty("county", "visibility", "visible");
   props.map.setLayoutProperty("town", "visibility", "none");
