57e837589093003363bb493e2caa09c81a6b1784
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-02-14 23:58:44 +0800

    fix: mapview missing element reference

--- src/components/view/MapView.vue
+++ src/components/view/MapView.vue
@@ -8,7 +8,14 @@ import MapReportMarker from "../component/MapReportMarker.vue";
 import MapRtsBox from "../component/MapRtsBox.vue";
 import MapRtsMarker from "../component/MapRtsMarker.vue";
 
-import { markRaw, onBeforeUnmount, onMounted, shallowRef, inject } from "vue";
+import {
+  markRaw,
+  onBeforeUnmount,
+  onMounted,
+  shallowRef,
+  inject,
+  ref,
+} from "vue";
 import type { Ref } from "vue";
 import { SettingsManager } from "tauri-settings";
 import maplibregl from "maplibre-gl";
@@ -34,6 +41,7 @@ defineProps<{
 }>();
 
 const map = shallowRef<maplibregl.Map | null>(null);
+const mapTemplate = ref<HTMLDivElement>(document.createElement("div"));
 const setting =
   inject<SettingsManager<DefaultConfigSchema>>("settings")?.settings;
 
@@ -42,7 +50,7 @@ onMounted(() => {
 
   map.value = markRaw(
     new maplibregl.Map({
-      container: "map",
+      container: mapTemplate.value,
       style: {
         version: 8,
         name: "TREM Map",
@@ -260,7 +268,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template lang="pug">
-#map.map-container.maplibregl-map(:class="{ 'hide-rts-markers': currentView.startsWith('report'), 'hide-report-list-markers': currentView != 'report-list' }")
+#map.map-container.maplibregl-map(ref="mapTemplate", :class="{ 'hide-rts-markers': currentView.startsWith('report'), 'hide-report-list-markers': currentView != 'report-list' }")
 .map-layers(v-if="map")
   .home(v-if="!Object.keys(eew).length && currentView == 'home'")
     MapHomeViewControl(:map="map")
