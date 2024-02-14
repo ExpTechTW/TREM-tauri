532a729dea77695503e2b86339b9a58cbcc017c3
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-02-15 01:07:06 +0800

    chore: more specific on font imports

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,15 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 }));
