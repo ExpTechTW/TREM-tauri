3a736980faf17b443c65c4973331b166d69ec09e
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-02-16 01:43:42 +0800

    fix: bundle target

--- vite.config.ts
+++ vite.config.ts
@@ -19,11 +19,12 @@ export default defineConfig(async () => ({
     },
   },
   build: {
+    target: "esnext",
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
           }
         }
       }
